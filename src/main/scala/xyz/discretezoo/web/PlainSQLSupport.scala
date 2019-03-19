9965bf1bb7c4e1174caffde2b60befb8475a4b05
Katja Berčič <katja.bercic@gmail.com>
2019-03-19 18:30:14 +0100

    Set up single object type, single collection case

--- src/main/scala/xyz/discretezoo/web/PlainSQLSupport.scala
+++ src/main/scala/xyz/discretezoo/web/PlainSQLSupport.scala
@@ -11,8 +11,8 @@ trait PlainSQLSupport[T <: ZooObject] {
   implicit val getResult: GetResult[T]
 
   def count(qp: SearchParam): DBIO[Int] = {
-    sql"""SELECT COUNT(*) FROM "#$tableName"
-                          WHERE #${plainGetWhere(qp)};""".as[Int].head
+    val where = if (plainGetWhere(qp).length > 0) s" WHERE ${plainGetWhere(qp)}" else ""
+    sql"""SELECT COUNT(*) FROM "#$tableName"#$where;""".as[Int].head
   }
 
   def get(qp: ResultParam): DBIO[Seq[T]] = {
@@ -36,7 +36,8 @@ trait PlainSQLSupport[T <: ZooObject] {
   private def plainGetWhere(sp: SearchParam): String = {
     val collections = sp.collections.map(inCollection.get)
       .collect({ case Some(v) => s"""("$v" IS NOT NULL)""" }).mkString(" OR ")
-    (sp.filter.map(plainFilter) :+ s"($collections)").mkString(" AND ")
+    val collectionCondition = if (collections.length > 0) s"($collections)" else ""
+    (sp.filter.map(plainFilter) :+ collectionCondition).mkString(" AND ")
   }
 
 }
