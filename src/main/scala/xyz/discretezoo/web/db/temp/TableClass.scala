4aff3b82faa5d0eb8cbe4a0d6c9dd89a8de6f3c1
Katja Berčič <katja.bercic@gmail.com>
2019-04-23 13:55:54 +0200

    Add foreign key placeholders

--- src/main/scala/xyz/discretezoo/web/db/temp/TableClass.scala
+++ src/main/scala/xyz/discretezoo/web/db/temp/TableClass.scala
@@ -5,8 +5,11 @@ import slick.collection.heterogeneous.HNil
 import slick.lifted.{ProvenShape, Rep}
 import xyz.discretezoo.web.DynamicSupport.ColumnSelector
 import xyz.discretezoo.web.ZooPostgresProfile.api._
+//joinImports
 
 final class %tableClass%(tag: Tag) extends Table[%caseClass%](tag, "%dbTableName%") with ColumnSelector {
+
+  //joinQueries
 
   def ID: Rep[UUID] = column[UUID]("ID", O.PrimaryKey)
 //accessorMethods
